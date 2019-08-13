89edcb0d8a41698e883392d3762788e6bc00d039
MICHAEL ASAMOAH <you@example.com>
2019-08-13 17:01:15 +0000

    initial commit

--- index.php
+++ index.php
@@ -1,6 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Quick Quiz</title>
+    <link rel="stylesheet" href="app.css" />
+    <link href="manifest.json" rel="manifest" />
+  </head>
+  <body>
+    <div class="container">
+      <div id="home" class="flex-center flex-column">
+        <h1>myIQ</h1>
+        <a class="btn" href="game.php">Play</a>
+        <a class="btn" href="highscores.php">High Scores</a>
+      </div>
+    </div>
+
+
     <script>
       // This is the service worker with the Cache-first network
 
@@ -22,20 +39,5 @@ if ("serviceWorker" in navigator) {
   }
 }
     </script>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>Quick Quiz</title>
-    <link rel="stylesheet" href="app.css" />
-    <link href="manifest.json" rel="manifest" />
-  </head>
-  <body>
-    <div class="container">
-      <div id="home" class="flex-center flex-column">
-        <h1>myIQ</h1>
-        <a class="btn" href="game.php">Play</a>
-        <a class="btn" href="highscores.php">High Scores</a>
-      </div>
-    </div>
   </body>
 </html>
